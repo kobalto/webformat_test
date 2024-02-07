b00f9bf3421b41a5f4920cd2acc7b73ab9f549d5
Fabio Giovannelli <fabio.giovannelli@didotech.com>
2024-02-07 06:40:56 +0100

    Aggiornato modelli

--- app/Http/Controllers/Api/DevPmController.php
+++ app/Http/Controllers/Api/DevPmController.php
@@ -5,13 +5,38 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\Employee;
+use Illuminate\Support\Arr;
 
 class DevPmController extends Controller
 {
     public function __invoke(Request $request, int $emp_id)
     {
+        $devs = Employee::Dev()->get()->toArray();
+        $dev_ids = Arr::pluck($devs , 'id');
+        if (!in_array($emp_id, $dev_ids))
+        {
+            return response()->json([
+                'message' => 'The employee id is not from a developer.'
+            ], 403);
+        }
+
+        
         $emp = Employee::find($emp_id);
+        
+        if ($emp->isDev() === false)
+        {
+            return response()->json([
+                'message' => 'The employee id is not from a developer.'
+            ], 403);
+        }
+
         $pm = Employee::find($emp->pm_id);
-        return $pm->firstname . ' '. $pm->lastname;
+
+        if ($pm)
+        {
+            return $pm->firstname . ' '. $pm->lastname;
+        }
+
+        return '';
     }
 }
--- app/Http/Controllers/Api/ProcessingTasksController.php
+++ app/Http/Controllers/Api/ProcessingTasksController.php
@@ -4,12 +4,23 @@ namespace App\Http\Controllers\Api;
 use Illuminate\Http\Request;
 use App\Models\Employee;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Arr;
 
 class ProcessingTasksController extends Controller
 {
-    public function __invoke(Request $request, int $dev_id)
+    public function __invoke(Request $request, int $emp_id)
     {
-        $dev = Employee::find($dev_id);
+        $devs = Employee::Dev()->get()->toArray();
+        $dev_ids = Arr::pluck($devs , 'id');
+        if (!in_array($emp_id, $dev_ids))
+        {
+            return response()->json([
+                'message' => 'The employee id is not from a developer.'
+            ], 403);
+        }
+
+
+        $dev = Employee::find($emp_id);
 
         if ($dev->isDev() === false)
         {
